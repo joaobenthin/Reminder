61a667a63907f7ae79ca93552c8208c89cb4c838
João Benthin <joaopaulobenthin@hotmail.com>
2025-02-27 20:32:13 -0300

    Criando sua primeira viewModel

---
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginBottomSheetViewModel.swift
+//  Reminder
+//
+//  Created by joaobenthin on 27/02/25.
+//
+
+import Foundation
+
+class LoginBottomSheetViewModel {
+    func doAuth(usernameLogin: String, password: String) {
+        print(usernameLogin)
+    }
+}
