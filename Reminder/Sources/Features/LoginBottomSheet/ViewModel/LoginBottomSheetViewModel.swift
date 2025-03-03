140e9c26ef999ffd5975834399584fc3a6d70441
João Benthin <joaopaulobenthin@hotmail.com>
2025-03-03 17:11:24 -0300

    Closures

--- Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -9,12 +9,15 @@ import Foundation
 import Firebase
 
 class LoginBottomSheetViewModel {
+    var successResult: (() -> Void)?
+    
     func doAuth(usernameLogin: String, password: String) {
         print(usernameLogin)
         Auth.auth().signIn(withEmail: usernameLogin, password: password) { [weak self] authResult, error in
             if let error = error {
                 print("Autentição não foi com sucesso \(error)")
             } else {
+                self?.successResult?()
                 print(authResult, "Sucesso na autenticação")
             }
             
