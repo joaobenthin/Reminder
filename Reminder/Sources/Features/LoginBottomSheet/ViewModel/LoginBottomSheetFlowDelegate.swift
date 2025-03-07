a3e7f724325d45d4c6ca8f3d4b1a15e910a2e19d
João Benthin <joaopaulobenthin@hotmail.com>
2025-03-07 16:55:22 -0300

    Estrutura da Navigation

---
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetFlowDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  LoginBottomSheetFlowDelegate.swift
+//  Reminder
+//
+//  Created by joaobenthin on 07/03/25.
+//
+
+import Foundation
+
+public protocol LoginBottomSheetFlowDelegate: AnyObject {
+    func navigateToHome()
+}
